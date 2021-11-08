3884f0c08c150d854d6f6737797e538814a2d905
Xaver F. Jandura <xaverjandura@gmail.com>
2021-11-08 14:06:38 +0100

    comment

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -4,30 +4,56 @@ namespace HelloWorld
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Jak se jmenuješ?");
-            //string name = Console.ReadLine();
+            ConvertFtoCandPrint();
 
-            //Hello(name);
-
-
-            Console.WriteLine("Zadej teplotu ve st. F.: ");
-            //string num = Console.ReadLine();
-
-            //int x = int.Parse(Console.ReadLine());
-
-            //int y = Calc(x);
-
-
-            double f = double.Parse(Console.ReadLine());
-            double c = Temper(f); 
-
-            //Console.WriteLine("Výsledek je: " + Calc(x));
-            Console.WriteLine("Výsledek je: " + Temper(f));
 
         }
 
+
+        /// <summary>
+        /// Convert temperature from F to C and print to console
+        /// </summary>
+        static void ConvertFtoCandPrint()
+        {
+            // vytvorte metodu ktera prevadi stupne F na C
+            // a prevadejte cisla ze vstupu od uzivatele
+
+            Console.WriteLine("Zadej teplotu ve stupnich F:");
+
+            double f = double.Parse(Console.ReadLine());
+            double c = ConvertFtoC(f);
+
+            Console.WriteLine($"{f}°F je {c}°C");
+        }
+
+        static void Hello(string name)
+        {
+            string name5p = name;
+
+            if (name == "Xaver")
+            {
+                name5p = "Xavere";
+            }
+            else if (name == "Karel")
+            {
+                name5p = "Karle";
+            }
+            else if (name == "Petr")
+            {
+                name5p = "Petře";
+            }
+            else
+            {
+                name5p += ", omlouvám se, ale tvoje jméno neumím skloňovat.";
+            }
+
+            string pozdrav = "Ahoj " + name5p;
+
+            Console.WriteLine(pozdrav);
+        }
 
         static int Calc(int x)
         {
@@ -37,36 +63,17 @@ namespace HelloWorld
             int result = x * 2 + 10;
 
             return result;
-
         }
 
-        static double Temper(double f)
+        /// <summary>
+        /// converts temperature from F to C
+        /// </summary>
+        /// <param name="tempF">temperature F as double</param>
+        /// <returns>temperature C</returns>
+        static double ConvertFtoC(double tempF)
         {
-            return (f - 32) / 1.8;
-        }
-
-            static void Hello(string name)
-        {
-            string name5p = name;
-
-            if (name == "Xaver")
-            {
-                name5p = "Xavere";
-            }
-            else if (name == "Iva")
-            {
-                name5p = "Ivo";
-            }
-            else if (name == "Petr")
-            {
-                name5p = "Petře";
-            }
-            else
-            {
-                name5p = name5p + ", promň, ale tvé jméno neumím skloňovat";
-            }
-            string pozdrav = "Ahoj " + name5p + "!";
-            Console.WriteLine(pozdrav);
+            return (tempF - 32) / 1.8;
         }
     }
+
 }
