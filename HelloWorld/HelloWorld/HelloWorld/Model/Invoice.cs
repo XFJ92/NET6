2e95f8fea502b2272a14e3426c5fe199a39a27b2
Xaver F. Jandura <xaverjandura@gmail.com>
2021-11-09 12:03:01 +0100

    vytvoreni tridy invoice

---
+++ HelloWorld/HelloWorld/HelloWorld/Model/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Model
+{
+    class Invoice
+    {
+        public string Target { get; set; }
+
+        public string Amount { get; set; }
+
+        public string Commodity { get; set; }
+
+    }
+}
