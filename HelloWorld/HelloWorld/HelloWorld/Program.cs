dd5c428701712f431ef9d7a7a11bf3a4ef5c26f4
Xaver F. Jandura <xaverjandura@gmail.com>
2021-11-10 14:33:52 +0100

    LINQ  zakladni priklady na operace s kolekci

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -14,45 +14,92 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
+            int[] numbers = { -2079, -498, 2920, -1856, 332, -2549, -674, -120, -992, 2782, 320, -524, 135, 952, 1868, 2509, -230, -138, -904, -480 };
+
+            /// z "numebers" zjistěte:
+            /// 1. počet prvků v poli
+            var count = numbers.Count();
+            Console.WriteLine(count);
+
+            /// 2. největší hodnotu
+            var max = numbers.Max();
+            Console.WriteLine(max);
+
+            /// 3. nejmenší hodnotu
+            var min = numbers.Min();
+            Console.WriteLine(min);
+
+            /// 4. průměr
+            var avg = numbers.Average();
+            Console.WriteLine(avg);
+
+            /// 5. kolik obsahuje pole kladných čísel
+
+            var positive = numbers
+                .Where(x => x >= 0).Count();
+            Console.WriteLine(positive);
+
+            /// 6. kolik obsahuje pole záporných čísel
+            var count_neg = numbers
+                .Where(x => x <= 0).Count();
+            Console.WriteLine(count_neg);
+
+            /// 7. sumu všech hodnot
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
+
+            /// 8. sumu kladných hodnot
+            var sum_pos = numbers
+                .Where(x => x >= 0).Sum();
+            Console.WriteLine(sum_pos);
 
 
 
-            int[] numbers = { 1, 2, -30, 4, 20, 256, -45, 99 };
-
-            //where - filtruje kolekci
 
 
             /*
+            //where - filtruje kolekci
             var results = numbers
                 .Where(number => number >= 0) //nebo lze zapsat v jednom dotazu: (number => number >= 0 && number <= 100)
                 .Where(number => number <= 100); //alternativa k zapisu dalsi podminky - retezeni podminek
-
             */
-
 
             //orderby = razeni
             //var results = numbers.OrderBy(sort => sort); //seradi nam prvky v kolekci od nejmensiho po nejvetsi
-
-
 
             //preskoceni nebo oseknuti kolekce > take, skip
 
             //var results = numbers.Take(5); //uvidim z kolekce prvnich 5 cisel
             //var results = numbers.Skip(5); //preskoci prvnich 5 cisel z kolekce - orizne je
 
-
             //var results = numbers.SkipWhile(n => n > 0); //podminka, ktera postupne prochazi kolekci, podiva se na 1 (podminka plati) a preskoci dal, podiva se na 2 (podminka plati) a zase jede dal, podiva se na -30 (podminka NEKPLATI) a vypise vsechno dal
 
 
+            /*
+            //agregacni operatory - max, min, sum, average
+            var sum = numbers.Sum();  //vraci nejakou hodnotu a ne kolekci, nebude tudiz fungovat foreach
+            Console.WriteLine(sum); //vypise sumu z kolekce
+
+            var avg = numbers.Average();
+            Console.WriteLine(avg);
+            */
 
 
+            /*
+            //vezme jeden prvek z kolekce - first, last
+            var first = numbers.First();
+            Console.WriteLine(first);
+            var last = numbers.Last();
+            Console.WriteLine(last);
+            */
 
 
-            foreach (var item in results) //jedna se o kolekci dat, takze k nim pristupujeme pres foreach
+            /*
+            foreach (var item in results) //jedna se o kolekci dat, takze k vyrazum z DB prevazne pristupujeme pres foreach
             {
                 Console.WriteLine(item);
             }
-
+            */
 
 
         }
